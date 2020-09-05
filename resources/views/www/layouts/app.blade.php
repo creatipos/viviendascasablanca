a416607f227697a7f561f9bce70e50d4a9db542d
Luis Daniel <creatiposweb@gmail.com>
2020-09-05 13:57:16 -0300

    dd

--- resources/views/www/layouts/app.blade.php
+++ resources/views/www/layouts/app.blade.php
@@ -31,6 +31,15 @@
 <link rel="stylesheet" href="{!! asset('consto/') !!}/css/bootstrap.min.css">
 <link rel="stylesheet" href="{!! asset('consto/') !!}/css/style.css">
 
+<!-- Global site tag (gtag.js) - Google Analytics -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=UA-177344840-1"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'UA-177344840-1');
+</script>
 
 </head>
 
